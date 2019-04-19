774e231eec2bf3ee2be1ab5d1135852347965a28
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-04-19 16:34:14 -0700

    Initialize project

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "TreehouseExample"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
---
+++ src/SuperHeroes.scala
@@ -0,0 +1,5 @@
+object SuperHeroes {
+  def main(args: Array [String]): Unit = {
+    println("Hello World")
+  }
+}
